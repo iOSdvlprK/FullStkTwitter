d53d5a9d8cad8bcb4a0d22221ac6df23166de6e1
iOS developer K <iOSdvlprK@gmail.com>
2023-07-08 23:18:57 +0900

    Initial Commit

---
+++ FullStkTwitter/FullStkTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullStkTwitterApp.swift
+//  FullStkTwitter
+//
+//  Created by joe on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct FullStkTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
