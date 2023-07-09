b7d8e171392f81a746f24d3975da7c88bbe20949
iOS developer K <iOSdvlprK@gmail.com>
2023-07-09 19:00:29 +0900

    TabView layout

---
+++ FullStkTwitter/View/Post/CreateTweetView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTweetView.swift
+//  FullStkTwitter
+//
+//  Created by joe on 2023/07/09.
+//
+
+import SwiftUI
+
+struct CreateTweetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTweetView()
+    }
+}
