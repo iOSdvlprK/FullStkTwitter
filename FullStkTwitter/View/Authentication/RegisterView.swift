47a05bc8b885e08dd5642c68af792c5c122d0bac
iOS developer K <iOSdvlprK@gmail.com>
2023-07-16 23:43:18 +0900

    Authentication: RegisterView

---
+++ FullStkTwitter/View/Authentication/RegisterView.swift
@@ -0,0 +1,80 @@
+//
+//  RegisterView.swift
+//  FullStkTwitter
+//
+//  Created by joe on 2023/07/16.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    @State private var name = ""
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.blue)
+                    })
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 35)
+            
+            VStack(alignment: .leading) {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            
+            Spacer(minLength: 0)
+            
+            VStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.gray)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                            .overlay {
+                                Text("Next")
+                                    .foregroundColor(.white)
+                            }
+                    })
+                }
+                .padding(.trailing, 24)
+            }
+        }
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
--- FullStkTwitter/View/Authentication/WelcomeView.swift
+++ FullStkTwitter/View/Authentication/WelcomeView.swift
@@ -9,112 +9,116 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Spacer(minLength: 0)
-                
-                Image("Twitter")
-                    .resizable()
-                    .scaledToFill()
-                    .padding(.trailing)
-                    .frame(width: 20, height: 20)
-                
-                Spacer(minLength: 0)
-            }
-            
-            Spacer(minLength: 0)
-            
-            Text("See what's happening in the world right now.")
-                .font(.system(size: 30, weight: .heavy, design: .default))
-                .frame(width: (UIScreen.main.bounds.width * 0.9), alignment: .center)
-            
-            Spacer(minLength: 0)
-            
-            VStack(alignment: .center, spacing: 10) {
-                Button(action: {
-                    print("Sign in with Google")
-                }, label: {
-                    HStack(spacing: -4) {
-                        Image("google")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        
-                        Text("Continue with Google")
-                            .fontWeight(.bold)
-                            .font(.title3)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 36)
-                            .stroke(Color.black, lineWidth: 1)
-                            .opacity(0.3)
-                            .frame(width: 320, height: 60, alignment: .center)
-                    }
-                })
-                
-                Button(action: {
-                    print("Sign in with Apple")
-                }, label: {
-                    HStack(spacing: -4) {
-                        Image("apple")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 25, height: 25)
-                        
-                        Text("Continue with Apple")
-                            .fontWeight(.bold)
-                            .font(.title3)
-                            .foregroundColor(.black)
-                            .padding()
-                    }
-                    .overlay {
-                        RoundedRectangle(cornerRadius: 36)
-                            .stroke(Color.black, lineWidth: 1)
-                            .opacity(0.3)
-                            .frame(width: 320, height: 60, alignment: .center)
-                    }
-                })
-                
+        NavigationView {
+            VStack {
                 HStack {
-                    Rectangle()
-                        .foregroundColor(.gray)
-                        .opacity(0.3)
-                        .frame(width: (UIScreen.main.bounds.width * 0.35), height: 1)
+                    Spacer(minLength: 0)
                     
-                    Text("Or")
-                        .foregroundColor(.gray)
+                    Image("Twitter")
+                        .resizable()
+                        .scaledToFill()
+                        .padding(.trailing)
+                        .frame(width: 20, height: 20)
                     
-                    Rectangle()
-                        .foregroundColor(.gray)
-                        .opacity(0.3)
-                        .frame(width: (UIScreen.main.bounds.width * 0.35), height: 1)
+                    Spacer(minLength: 0)
                 }
                 
-                RoundedRectangle(cornerRadius: 36)
-                    .foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
-                    .frame(width: 320, height: 60, alignment: .center)
-                    .overlay {
-                        Text("Create account")
-                            .fontWeight(.bold)
-                            .font(.title3)
-                            .foregroundColor(.white)
-                            .padding()
+                Spacer(minLength: 0)
+                
+                Text("See what's happening in the world right now.")
+                    .font(.system(size: 30, weight: .heavy, design: .default))
+                    .frame(width: (UIScreen.main.bounds.width * 0.9), alignment: .center)
+                
+                Spacer(minLength: 0)
+                
+                VStack(alignment: .center, spacing: 10) {
+                    Button(action: {
+                        print("Sign in with Google")
+                    }, label: {
+                        HStack(spacing: -4) {
+                            Image("google")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                            
+                            Text("Continue with Google")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 36)
+                                .stroke(Color.black, lineWidth: 1)
+                                .opacity(0.3)
+                                .frame(width: 320, height: 60, alignment: .center)
+                        }
+                    })
+                    
+                    Button(action: {
+                        print("Sign in with Apple")
+                    }, label: {
+                        HStack(spacing: -4) {
+                            Image("apple")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 25, height: 25)
+                            
+                            Text("Continue with Apple")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 36)
+                                .stroke(Color.black, lineWidth: 1)
+                                .opacity(0.3)
+                                .frame(width: 320, height: 60, alignment: .center)
+                        }
+                    })
+                    
+                    HStack {
+                        Rectangle()
+                            .foregroundColor(.gray)
+                            .opacity(0.3)
+                            .frame(width: (UIScreen.main.bounds.width * 0.35), height: 1)
+                        
+                        Text("Or")
+                            .foregroundColor(.gray)
+                        
+                        Rectangle()
+                            .foregroundColor(.gray)
+                            .opacity(0.3)
+                            .frame(width: (UIScreen.main.bounds.width * 0.35), height: 1)
                     }
-            }
-            .padding()
-            
-            VStack(alignment: .leading) {
-                VStack {
-                    Text("By signing up, you agree to our ") + Text("Terms").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255)) + Text(", ") + Text("Privacy Policy").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255)) + Text(", and Cookie Use.")
+                    
+                    RoundedRectangle(cornerRadius: 36)
+                        .foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                        .frame(width: 320, height: 60, alignment: .center)
+                        .overlay {
+                            Text("Create account")
+                                .fontWeight(.bold)
+                                .font(.title3)
+                                .foregroundColor(.white)
+                                .padding()
+                        }
                 }
-                .padding(.bottom)
+                .padding()
                 
-                HStack(spacing: 2) {
-                    Text("Have an account already? ") + Text("Log in").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                VStack(alignment: .leading) {
+                    VStack {
+                        Text("By signing up, you agree to our ") + Text("Terms").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255)) + Text(", ") + Text("Privacy Policy").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255)) + Text(", and Cookie Use.")
+                    }
+                    .padding(.bottom)
+                    
+                    HStack(spacing: 2) {
+                        Text("Have an account already? ") + Text("Log in").foregroundColor(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                    }
                 }
             }
+            .navigationBarHidden(true)
+            .navigationBarTitle("")
         }
     }
 }
