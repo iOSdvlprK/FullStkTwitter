349cebafc9697e77cb807343e6fedc199b1515c9
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-08-17 23:56:27 +0900

    CreateTweet: using ImagePicker in SwiftUI

--- FullStkTwitter/View/Post/CreateTweet.swift
+++ FullStkTwitter/View/Post/CreateTweet.swift
@@ -13,6 +13,11 @@ struct CreateTweet: View {
     
     @ObservedObject var viewModel = CreateTweetViewModel()
     
+    @State var imagePickerPresented = false
+    @State var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var width = UIScreen.main.bounds.width
+    
     var body: some View {
         VStack {
             HStack {
@@ -40,8 +45,49 @@ struct CreateTweet: View {
             }
             
             MultilineTextField(text: $text)
+            
+            if postImage == nil {   // image not chosen
+                Button {
+                    self.imagePickerPresented.toggle()
+                } label: {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 70, height: 70)
+                        .clipped()
+                        .padding(.top)
+                        .foregroundColor(.black)
+                }
+                .sheet(isPresented: $imagePickerPresented) {
+                    loadImage()
+                } content: {
+                    ImagePicker(image: $selectedImage)
+                }
+            }
+            else if let image = postImage { // image chosen
+                VStack {
+                    HStack(alignment: .top) {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.horizontal)
+                            .frame(width: width * 0.9)
+                            .cornerRadius(16)
+                            .clipped()
+                    }
+                    
+                    Spacer()
+                }
+            }
         }
         .padding()
+    }
+}
+
+extension CreateTweet {
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+        postImage = Image(uiImage: selectedImage)
     }
 }
 
